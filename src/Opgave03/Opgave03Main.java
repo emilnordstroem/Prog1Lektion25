589a2d8a83e94a65b53024e41630033073c1f4a4
Benn Christensen <bnch@eaaa.dk>
2023-12-07 10:01:16 +0100

    added exercise03

---
+++ src/Opgave03/Opgave03Main.java
@@ -0,0 +1,8 @@
+package Opgave03;
+
+public class Opgave03Main {
+    public static void main(String[] args) {
+        int[] intArray1 = {2, 4, 6, 8, 10, 12, 14};
+        int[] intArray2 = {1, 2, 4, 5, 6, 9, 12, 17};
+    }
+}
